276f740cbff193827910cb649234127825424f0b
Muhammed Saad <muhammedsaad007700@gmail.com>
2026-06-03 20:40:19 +0530

    Time: 2 ms (75%), Space: 150.7 MB (0%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.dart
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.dart
@@ -1,5 +1,15 @@
 class Solution {
   int countPairs(List<int> nums, int k) {
-    
+    int count =0;
+    for(int i=0;i<nums.length;i++){
+        for(int j=i;j<nums.length;j++){
+            if(i<j && nums[i] == nums[j]){
+                if((i*j)%k ==0){
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
   }
 }
