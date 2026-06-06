52dedb22353d90577c8448d31e407f82ff096081
Muhammed Saad <muhammedsaad007700@gmail.com>
2026-06-06 19:32:34 +0530

    Time: 0 ms (100%), Space: 154.4 MB (11.11%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.dart
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   int val;
+ *   TreeNode? left;
+ *   TreeNode? right;
+ *   TreeNode([this.val = 0, this.left, this.right]);
+ * }
+ */
+class Solution {
+  TreeNode? searchBST(TreeNode? root, int val) {
+    if(root == null)return null;
+
+    if(root.val == val){
+        return root;
+    }
+
+    if(val < root.val){
+        return searchBST(root.left,val);
+    }
+
+    return searchBST(root.right,val);
+  }
+}
